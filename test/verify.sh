ae70785b326964e5d79bc8eb8c0957a8b170e6ba
Gabor Szabo <szgabsz91@gmail.com>
2019-09-21 16:43:28 +0200

    feat: #15 support JDK 13 (#16)

--- test/verify.sh
+++ test/verify.sh
@@ -17,8 +17,8 @@ function assertVersion() {
 }
 
 echo "Checking versions"
-assertVersion "java --version" openjdk[[:space:]]12\.
-assertVersion "javac --version" javac[[:space:]]12\.
+assertVersion "java --version" openjdk[[:space:]]13\.
+assertVersion "javac --version" javac[[:space:]]13\.
 assertVersion "python --version" Python[[:space:]]2\.
 assertVersion "ocamorph --version" 0.1
 

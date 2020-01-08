345f4b2dbbee02a018ae9f0e9a206b3aeae40d82
Gabor Szabo <szgabsz91@gmail.com>
2020-01-08 22:34:12 +0100

    chore: #19 update Travis pipeline

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,8 +2,8 @@
 
 function publish() {
     echo "Publishing for version $1"
-    docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
     docker tag jdk-ocamorph-pyphen szgabsz91/jdk-ocamorph-pyphen:$1
+    echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
     docker push szgabsz91/jdk-ocamorph-pyphen:$1
 }
 

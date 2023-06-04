5096e4556472d9c6523df145209adba0f4dc5692
Gabor Szabo <szgabsz91@gmail.com>
2023-06-04 10:19:42 +0200

    chore: #37 migrate from TravisCI to CircleCI

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # jdk-ocamorph-pyphen
 
-[![Build Status](https://travis-ci.com/szgabsz91/jdk-ocamorph-pyphen.svg?branch=master)](https://app.travis-ci.com/github/szgabsz91/jdk-ocamorph-pyphen)
+[![Build Status](https://img.shields.io/circleci/project/github/szgabsz91/jdk-ocamorph-pyphen/master.svg)](https://circleci.com/gh/szgabsz91/workflows/jdk-ocamorph-pyphen)
 
 Docker image that contains JDK, Ocamorph and Pyphen.
 
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,25 +1,6 @@
 #!/usr/bin/env bash
 
-function publish() {
-    echo "Publishing for version $1"
-    docker tag jdk-ocamorph-pyphen szgabsz91/jdk-ocamorph-pyphen:$1
-    echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-    docker push szgabsz91/jdk-ocamorph-pyphen:$1
-}
-
-BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-${TRAVIS_BRANCH}}
-
-echo "TRAVIS_TAG: $TRAVIS_TAG"
-echo "TRAVIS_PULL_REQUEST_BRANCH: $TRAVIS_PULL_REQUEST_BRANCH"
-echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
-echo "BRANCH: $BRANCH"
-
-if [[ $BRANCH == "master" ]]; then
-    # master branch - latest version
-    publish "latest"
-elif [ ! -z "$TRAVIS_TAG" ]; then
-    # tag - cut version from the tag
-    publish $(echo $TRAVIS_TAG | cut -d '-' -f 4)
-else
-    echo "Not pushing Docker image from a feature branch"
-fi
+echo "Publishing for version $1"
+docker tag jdk-ocamorph-pyphen szgabsz91/jdk-ocamorph-pyphen:$1
+echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+docker push szgabsz91/jdk-ocamorph-pyphen:$1
